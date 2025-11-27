d2f0d0f99939ff18d8a3b26c45e0bd007a0d34ce
Meir Iluz <Meiriluz12@gmail.com>
2025-11-27 15:07:25 +0200

    updated all the prints into a logger usage

--- src/model/data_classes/Truck.py
+++ src/model/data_classes/Truck.py
@@ -1,6 +1,17 @@
 from infrastructure.interfaces.ivehicle import IVehicle
+from infrastructure.factories.logger_factory import LoggerFactory
+from globals.consts.const_strings import ConstStrings
+from globals.consts.logger_messages import LoggerMessages
 
 
 class Truck(IVehicle):
+    def __init__(self):
+        self._logger = LoggerFactory.get_logger_manager()
+
     def start_engine(self):
-        print("I'm a Truck")
+        vehicle_name = self.__class__.__name__
+        
+        self._logger.log(
+            ConstStrings.LOG_NAME_DEBUG,
+            LoggerMessages.VEHICLE_ENGINE_MESSAGE.format(vehicle_name)
+        )
