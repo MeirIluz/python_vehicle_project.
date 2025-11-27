e2ccd0b1d780a178298893d0896f3be40772162f
Meir Iluz <Meiriluz12@gmail.com>
2025-11-27 08:04:08 +0200

    simplified main to only call vehicle factory

--- src/main.py
+++ src/main.py
@@ -3,15 +3,7 @@ from infrastructure.factories.vehicle_factory import VehicleFactory
 
 def main():
 
-    v1 = VehicleFactory.create_vehicle("car")
-    v2 = VehicleFactory.create_vehicle("truck")
-    v3 = VehicleFactory.create_vehicle("motorcycle")
-    v4 = VehicleFactory.create_vehicle("plane")
-
-    v1.start_engine()
-    v2.start_engine()
-    v3.start_engine()
-    v4.start_engine()
+    VehicleFactory.create_all()
 
 
 if __name__ == "__main__":
